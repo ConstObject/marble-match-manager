34f962e3d6c83a6c3821b501b8c81b2002f92121
WannaDie <consts7s@gmail.com>
2021-04-27 11:15:14 -0400

    Added ability for me to init servers Added command to print current version and features/updates

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ import database_operation
 from discord.ext import commands
 
 
+def has_database_permission():
+    async def predicate(ctx):
+        return ctx.author.id == 126913881879740416
+    return commands.check(predicate)
+
+
 class InitCog(commands.Cog, name='Initializations'):
 
     def __init__(self, bot):
@@ -23,10 +29,25 @@ class InitCog(commands.Cog, name='Initializations'):
 
     @commands.command(name='init', help='Adds all server members to the database if they do not exist already')
     @commands.guild_only()
-    @commands.has_role('Admin')
+    @has_database_permission()
     async def init(self, ctx):
         for members in ctx.guild.members:
             if database_operation.get_player_id(database.db_connection, str(members), ctx.guild.id) is None:
                 database_operation.create_user(database.db_connection, None, str(members), 10, ctx.guild.id)
                 print(f'Added {members.name} to database')
         await ctx.send('Any members not added to the database have been added')
+
+    @commands.command(name='info', help='Prints latest version info')
+    @commands.guild_only()
+    async def info(self, ctx):
+        text = f'```\n' \
+               f'Current version 1.0a\n' \
+               f'Features:\n' \
+               f'\t-Create marble matches with other discord members\n' \
+               f'\t-Bet on others matches\n' \
+               f'\t-View statistics about the server economy\n' \
+               f'\t-Transfer marbles to other users\n' \
+               f'\t-View match/bet history and stats from marble matches\n' \
+               f'```'
+
+        await ctx.send(text)
