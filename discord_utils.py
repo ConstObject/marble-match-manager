bd8d7793f9d95d4c97d907f4af37da2a75a7177a
WannaDie <consts7s@gmail.com>
2021-04-27 09:57:20 -0400

    Added function to get discord member by player_id

--- discord_utils.py
+++ discord_utils.py
@@ -1,5 +1,21 @@
 import discord
+import database
+import database_operation
 
 
-async def code_message(ctx, text):
+async def code_message(ctx, text, color=0):
     await ctx.send(f'```\n{text}```')
+
+
+def get_member_by_player_id(ctx, connection, player_id):
+
+    player_info = database_operation.get_player_info(connection, player_id)
+
+    if player_info is None:
+        return 0
+
+    for member in ctx.guild.members:
+        if str(member) == player_info[1]:
+            return member
+
+    return 0
