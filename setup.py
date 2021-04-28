45dcf57d0c7fe0f1a7a81b482ba244fd22f7f751
WannaDie <consts7s@gmail.com>
2021-04-28 10:29:19 -0400

    Added docstrings to functions Added annotations to functions

--- setup.py
+++ setup.py
@@ -6,14 +6,22 @@ from discord.ext import commands
 
 
 def has_database_permission():
-    async def predicate(ctx):
+    async def predicate(ctx: commands.Context):
+        """Returns true if is me or has admin role
+
+        **Arguments**
+
+        - `<ctx>` Context to check.
+
+        """
+
         return ctx.author.id == 126913881879740416 or commands.has_role('Admin')
     return commands.check(predicate)
 
 
 class InitCog(commands.Cog, name='Initializations'):
 
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.Cog.listener()
@@ -23,7 +31,7 @@ class InitCog(commands.Cog, name='Initializations'):
         await self.bot.change_presence(activity=discord.Game(name='$help'))
 
     @commands.Cog.listener()
-    async def on_member_join(self, member):
+    async def on_member_join(self, member: discord.Member):
         if not database_operation.get_player_id(database.db_connection, str(member), member.guild.id):
             database_operation.create_user(database.db_connection, None, str(member), 10, member.guild.id)
             print(f'Added {member.name} to database')
@@ -31,7 +39,18 @@ class InitCog(commands.Cog, name='Initializations'):
     @commands.command(name='init', help='Adds all server members to the database if they do not exist already')
     @commands.guild_only()
     @has_database_permission()
-    async def init(self, ctx):
+    async def init(self, ctx: commands.Context):
+        """Adds server member to database if they do not exist in database
+
+        Examples:
+            - `$init`
+
+        **Arguments**
+
+        - `<ctx>` Context used to send confirmations.
+
+        """
+
         for members in ctx.guild.members:
             if not du.get_id_by_member(ctx, database.db_connection, members):
                 database_operation.create_user(database.db_connection, None, str(members), 10, ctx.guild.id)
@@ -40,7 +59,18 @@ class InitCog(commands.Cog, name='Initializations'):
 
     @commands.command(name='info', help='Prints latest version info')
     @commands.guild_only()
-    async def info(self, ctx):
+    async def info(self, ctx: commands.Context):
+        """Prints latest version info
+
+        Examples:
+            - `$info`
+
+        **Arguments**
+
+        - `<ctx>` Context used to send confirmations.
+
+        """
+
         text = f'```\n' \
                f'Current version 1.0a\n' \
                f'Features:\n' \
