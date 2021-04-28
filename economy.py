a5100f56471ba1f38ac285c68b55c18bf49c6a93
WannaDie <consts7s@gmail.com>
2021-04-28 10:08:27 -0400

    Added docstrings to economy Added function annotations

--- economy.py
+++ economy.py
@@ -8,7 +8,7 @@ import numpy as np
 
 class EconCog(commands.Cog, name='Marbles'):
 
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     # def testing(self):
@@ -17,7 +17,20 @@ class EconCog(commands.Cog, name='Marbles'):
     @commands.command(name='set_marbles', help='Will set the users marble count to a new number')
     @commands.guild_only()
     @commands.has_role('Admin')
-    async def set_marbles(self, ctx, member: discord.Member, marbles: int):
+    async def set_marbles(self, ctx: commands.Context, member: discord.Member, marbles: int):
+        """Sets a users marbles to a specified amount
+
+        Examples:
+            - `$set_marbles @Sophia 10`
+            - `$set_marbles @Ness 0`
+
+        **Arguments**
+
+        - `<ctx>` The context used to send confirmations.
+        - `<member>` The user who's marbles you want to set.
+        - `<marbles>` Amount to set users.
+
+        """
 
         if marbles < 0:
             await du.code_message(ctx, 'You cannot set a users marbles to any negative number')
@@ -30,7 +43,20 @@ class EconCog(commands.Cog, name='Marbles'):
     @commands.command(name='add_marbles', help='Will add to the users marble bank')
     @commands.guild_only()
     @commands.has_role('Admin')
-    async def add_marbles(self, ctx, member: discord.Member, marbles: int):
+    async def add_marbles(self, ctx: commands.Context, member: discord.Member, marbles: int):
+        """Add marbles to a users bank
+
+        Examples:
+            - `$add_marbles @Sophia 10`
+            - `$add_marbles @Ness 5`
+
+        **Arguments**
+
+        - `<ctx>` The context used to send confirmations.
+        - `<member>` User you want to add marbles to.
+        - `<marbles>` Amount to add to users bank.
+
+        """
 
         if marbles < 1:
             await du.code_message(ctx, 'You cannot add non positive numbers to a users marble bank')
@@ -47,7 +73,20 @@ class EconCog(commands.Cog, name='Marbles'):
     @commands.command(name='subtract_marbles', help='Will subtract from the users marble bank')
     @commands.guild_only()
     @commands.has_role('Admin')
-    async def sub_marbles(self, ctx, member: discord.Member, marbles: int):
+    async def sub_marbles(self, ctx: commands.Context, member: discord.Member, marbles: int):
+        """Subtracts marbles from a users bank
+
+        Examples:
+            - `$subtract_marbles @Sophia 10`
+            - `$subtract_marbles @Ness 1`
+
+        **Arguments**
+
+        - `<ctx>` The context used to send confirmations.
+        - `<member>` The member you'd like to subtract marbles from.
+        - `<marbles>` Amount of marbles to subtract.
+
+        """
 
         if marbles < 1:
             await du.code_message(ctx, 'You cannot subtract non positive numbers to a users marble bank')
@@ -63,7 +102,20 @@ class EconCog(commands.Cog, name='Marbles'):
 
     @commands.command(name='marbles', help='Prints their marble count')
     @commands.guild_only()
-    async def balance(self, ctx, member: discord.Member = None):
+    async def balance(self, ctx: commands.Context, member: discord.Member = None):
+        """Prints a users marble balance
+
+        Examples:
+            - `$marbles @Sophia`
+            - `$marbles`
+
+        **Arguments**
+
+        - `<ctx>` The context used to send confirmations.
+        - `<member>` The member who's marble count you want to see. If omitted defaults to your own marbles.
+
+        """
+
         if member is None:
             player_id = du.get_id_by_member(ctx, database.db_connection, ctx.author)
             await du.code_message(ctx,
@@ -77,7 +129,21 @@ class EconCog(commands.Cog, name='Marbles'):
 
     @commands.command(name='transfer', help='Transfers marbles from your bank to theirs')
     @commands.guild_only()
-    async def transfer(self, ctx, member: discord.Member, marbles: int):
+    async def transfer(self, ctx: commands.Context, member: discord.Member, marbles: int):
+        """Transfers marbles from your bank to another users bank
+
+        Examples:
+            - `$transfer @Sophia 10`
+            - `$transfer @Ness 5`
+
+        **Arguments**
+
+        - `<ctx>` The context used to send confirmations.
+        - `<member>` User you want to transfer marbles to.
+        - `<marbles>` Amount to transfer to a user from your bank
+
+        """
+
         if ctx.author == member:
             await du.code_message(ctx, 'You cannot send marbles to yourself')
             return
@@ -104,7 +170,18 @@ class EconCog(commands.Cog, name='Marbles'):
 
     @commands.command(name='economy', help='Summery of the server economy')
     @commands.guild_only()
-    async def summery(self, ctx):
+    async def summery(self, ctx: commands.Context):
+        """Prints a simple summery of the server economy
+
+        Examples:
+            - `$economy`
+
+        **Arguments**
+
+        - `<ctx>` The context used to get server information.
+
+        """
+
         players = database_operation.get_player_info_all_by_server(database.db_connection, ctx.guild.id)
         marbles = 0
         for user in players:
@@ -114,7 +191,18 @@ class EconCog(commands.Cog, name='Marbles'):
 
     @commands.command(name='summery', help='Prints a more detailed summery of the server economy')
     @commands.guild_only()
-    async def ex_summery(self, ctx):
+    async def ex_summery(self, ctx: commands.Context):
+        """Prints a detailed summery of the server economy
+
+        Examples:
+            - `$summery`
+
+        **Arguments**
+
+        - `<ctx>` The context used to get server information.
+
+        """
+
         count = 0
         sum_marbles = 0
         marbles = []
@@ -138,8 +226,8 @@ class EconCog(commands.Cog, name='Marbles'):
         gini = ((np.sum((2 * index - n - 1) * array)) / (n * np.sum(array)))
 
         await du.code_message(ctx, f'There are currently {sum_marbles} marbles in circulation\n'
-                                f'The current mean marble count is {int(sum_marbles / count)}\n'
-                                f'Inequality index is {gini}\n')
+                                   f'The current mean marble count is {int(sum_marbles / count)}\n'
+                                   f'Inequality index is {gini}\n')
 
 
 def setup(bot):
