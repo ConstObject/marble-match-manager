96c9e77b62fab51982e3e2b9d05e60f2d810a23a
WannaDie <consts7s@gmail.com>
2021-04-28 09:20:09 -0400

    Added docstrings to discord_utils Added function annotations

--- discord_utils.py
+++ discord_utils.py
@@ -5,12 +5,29 @@ import database_operation
 from discord.ext import commands
 
 
-async def code_message(ctx, text, color=0):
+async def code_message(ctx: commands.Context, text: str, color: int = 0):
+    """Takes ctx and sends message back to the user
+
+    **Arguments**
+
+    - `<ctx>` Context used to send the message.
+    - `<text>` Text to send.
+    - `<color>` Currently not implemented.
+
+    """
     return await ctx.send(f'```\n{text}```')
 
 
-def get_member_by_player_id(ctx, connection, player_id):
+def get_member_by_player_id(ctx: commands.Context, connection: sqlite3.Connection, player_id: int):
+    """Returns discord.Member associated with player_id in database
 
+    **Arguments**
+
+    - `<ctx>` Context used to get server id and member list of server.
+    - `<connection>` sqlite3 connection to read from database.
+    - `<player_id>` Database index of player you to get discord.Member from.
+
+    """
     player_info = database_operation.get_player_info(connection, player_id)
 
     if player_info is None:
@@ -24,7 +41,15 @@ def get_member_by_player_id(ctx, connection, player_id):
 
 
 def get_id_by_member(ctx: commands.Context, connection: sqlite3.Connection, member: discord.Member) -> int:
+    """Returns player_id associated with discord.Member
 
+    **Arguments**
+
+    - `<ctx>` Context used to get server of the discord.Member.
+    - `<connection>` sqlite3 connection to read from database.
+    - `<member>` Member who's player id you'd like to receive.
+
+    """
     if isinstance(ctx.channel, discord.DMChannel):
         return 0
 
