0de4caa817d25094b267165a2738bf8d37485bf2
WannaDie <consts7s@gmail.com>
2021-04-28 14:56:15 -0400

    Added function annotations

--- database_operation.py
+++ database_operation.py
@@ -5,7 +5,9 @@ import database
 from sqlite3 import Error
 
 
-def create_user(connection, player_id, username, marbles, server_id, wins=0, loses=0):
+def create_user(connection: sqlite3.Connection, player_id: int, username: str, marbles: int, server_id: int,
+                wins: int = 0, loses: int = 0) -> int:
+
     cur = connection.cursor()
     cur.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)", [player_id, username, marbles, server_id, wins, loses])
 
@@ -13,7 +15,9 @@ def create_user(connection, player_id, username, marbles, server_id, wins=0, los
     return cur.lastrowid
 
 
-def create_match(connection, match_id, amount, active, participant1, participant2):
+def create_match(connection: sqlite3.Connection, match_id: int, amount: int, active: int,
+                 participant1: int, participant2: int) -> int:
+
     cur = connection.cursor()
     cur.execute("INSERT INTO matches VALUES (?, ?, ?, ?, ?, ?)",
                 [match_id, amount, active, participant1, participant2, 0])
@@ -22,7 +26,9 @@ def create_match(connection, match_id, amount, active, participant1, participant
     return cur.lastrowid
 
 
-def create_bet(connection, bet_id, amount, match_id, better_id, participant1):
+def create_bet(connection: sqlite3.Connection, bet_id: int, amount: int, match_id: int, better_id: int,
+               participant1: int):
+
     cur = connection.cursor()
     cur.execute("INSERT INTO bets VALUES (?, ?, ?, ?, ?)", [bet_id, amount, match_id, better_id, participant1])
 
@@ -31,7 +37,9 @@ def create_bet(connection, bet_id, amount, match_id, better_id, participant1):
     return cur.lastrowid
 
 
-def create_match_history(connection, match_id, amount, participant1, participant2, winner_id, time):
+def create_match_history(connection: sqlite3.Connection, match_id: int, amount: int,
+                         participant1: int, participant2: int, winner_id: int, time: datetime.datetime):
+
     cur = connection.cursor()
     cur.execute("INSERT INTO matches_history VALUES (?, ?, ?, ?, ?, ?)",
                 [match_id, amount, participant1, participant2, winner_id, time])
@@ -41,7 +49,9 @@ def create_match_history(connection, match_id, amount, participant1, participant
     return cur.lastrowid
 
 
-def create_bet_history(connection, bet_id, amount, match_id, better_id, participant1, winner_id, time):
+def create_bet_history(connection: sqlite3.Connection, bet_id: int, amount: int, match_id: int, better_id: int,
+                       participant1: int, winner_id: int, time: datetime.datetime):
+
     cur = connection.cursor()
     cur.execute("INSERT INTO bets_history VALUES (?, ?, ?, ?, ?, ?, ?)",
                 [bet_id, amount, match_id, better_id, participant1, winner_id, time])
@@ -51,49 +61,56 @@ def create_bet_history(connection, bet_id, amount, match_id, better_id, particip
     return cur.lastrowid
 
 
-def update_match_activity(connection, match_id, active=1):
+def update_match_activity(connection: sqlite3.Connection, match_id: int, active: int = 1):
+
     cur = connection.cursor()
     cur.execute("UPDATE matches SET active = ? WHERE id = ?", [active, match_id])
 
     connection.commit()
 
 
-def update_match_accepted(connection, match_id, accepted=1):
+def update_match_accepted(connection: sqlite3.Connection, match_id: int, accepted: int = 1):
+
     cur = connection.cursor()
     cur.execute("UPDATE matches SET accepted = ? WHERE id = ?", [accepted, match_id])
 
     connection.commit()
 
 
-def update_marble_count(connection, player_id, marbles):
+def update_marble_count(connection: sqlite3.Connection, player_id: int, marbles: int):
+
     cur = connection.cursor()
     cur.execute("UPDATE users SET marbles = ? WHERE id = ?", [marbles, player_id])
 
     connection.commit()
 
 
-def update_player_wins(connection, player_id, wins):
+def update_player_wins(connection: sqlite3.Connection, player_id: int, wins: int):
+
     cur = connection.cursor()
     cur.execute("UPDATE users SET wins = ? WHERE id = ?", [wins, player_id])
 
     connection.commit()
 
 
-def update_player_loses(connection, player_id, loses):
+def update_player_loses(connection: sqlite3.Connection, player_id: int, loses: int):
+
     cur = connection.cursor()
     cur.execute("UPDATE users SET loses = ? WHERE id = ?", [loses, player_id])
 
     connection.commit()
 
 
-def update_bet(connection, bet_id, player_id, amount):
+def update_bet(connection: sqlite3.Connection, bet_id: int, player_id: int, amount: int):
+
     cur = connection.cursor()
     cur.execute("UPDATE bets SET amount=?, participant1=? WHERE id=?", [amount, player_id, bet_id])
 
     connection.commit()
 
 
-def get_match_info_by_id(connection, match_id):
+def get_match_info_by_id(connection: sqlite3.Connection, match_id: int) -> int:
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM matches WHERE id=?", [match_id])
 
@@ -105,7 +122,8 @@ def get_match_info_by_id(connection, match_id):
         return 0
 
 
-def get_match_history_info(connection, match_id):
+def get_match_history_info(connection: sqlite3.Connection, match_id: int):
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM matches_history WHERE id=?", [match_id])
 
@@ -117,7 +135,8 @@ def get_match_history_info(connection, match_id):
         return 0
 
 
-def get_match_history_info_all(connection, player_id):
+def get_match_history_info_all(connection: sqlite3.Connection, player_id: int):
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM matches_history WHERE participant1=? OR participant2=?", [player_id, player_id])
 
@@ -129,7 +148,8 @@ def get_match_history_info_all(connection, player_id):
         return 0
 
 
-def get_player_id(connection, username, server_id):
+def get_player_id(connection: sqlite3.Connection, username: str, server_id: int) -> int:
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM users WHERE username=? AND server_id=?", [username, server_id])
 
@@ -141,7 +161,8 @@ def get_player_id(connection, username, server_id):
         return 0
 
 
-def get_player_info(connection, player_id):
+def get_player_info(connection: sqlite3.Connection, player_id: int):
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM users WHERE id=?", [player_id])
 
@@ -153,16 +174,19 @@ def get_player_info(connection, player_id):
         return 0
 
 
-def get_player_wins(connection, player_id):
+def get_player_wins(connection: sqlite3.Connection, player_id: int) -> int:
+
     sqlquery = get_player_info(connection, player_id)
     return sqlquery[4]
 
 
-def get_player_loses(connection, player_id):
+def get_player_loses(connection: sqlite3.Connection, player_id: int) -> int:
+
     return get_player_info(connection, player_id)[5]
 
 
-def get_player_info_all_by_server(connection, server_id):
+def get_player_info_all_by_server(connection: sqlite3.Connection, server_id: int):
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM users WHERE server_id=?", [server_id])
 
@@ -174,7 +198,8 @@ def get_player_info_all_by_server(connection, server_id):
         return 0
 
 
-def get_marble_count(connection, player_id):
+def get_marble_count(connection: sqlite3.Connection, player_id: int) -> int:
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM users WHERE id=?", [player_id])
 
@@ -186,7 +211,8 @@ def get_marble_count(connection, player_id):
         return 0
 
 
-def get_bet_info(connection, bet_id):
+def get_bet_info(connection: sqlite3.Connection, bet_id: int):
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM bets WHERE id=?", [bet_id])
 
@@ -198,7 +224,8 @@ def get_bet_info(connection, bet_id):
         return 0
 
 
-def get_bet_info_all(connection, match_id):
+def get_bet_info_all(connection: sqlite3.Connection, match_id: int):
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM bets WHERE match_id=?", [match_id])
 
@@ -210,7 +237,8 @@ def get_bet_info_all(connection, match_id):
         return 0
 
 
-def get_bet_history_info(connection, bet_id):
+def get_bet_history_info(connection: sqlite3.Connection, bet_id: int):
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM bets_history WHERE id=?", [bet_id])
 
@@ -222,7 +250,8 @@ def get_bet_history_info(connection, bet_id):
         return 0
 
 
-def get_bet_history_info_all(connection, better_id):
+def get_bet_history_info_all(connection: sqlite3.Connection, better_id: int):
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM bets_history WHERE better_id=?", [better_id])
 
@@ -234,7 +263,8 @@ def get_bet_history_info_all(connection, better_id):
         return 0
 
 
-def find_match_by_player_id(connection, player_id):
+def find_match_by_player_id(connection: sqlite3.Connection, player_id: int):
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM matches WHERE participant1=? OR participant2=?", [player_id, player_id])
     sqlquery = cur.fetchone()
@@ -245,7 +275,8 @@ def find_match_by_player_id(connection, player_id):
         return 0
 
 
-def find_bet(connection, match_id, better_id):
+def find_bet(connection: sqlite3.Connection, match_id: int, better_id: int):
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM bets WHERE match_id=? AND better_id=?", [match_id, better_id])
 
@@ -257,45 +288,52 @@ def find_bet(connection, match_id, better_id):
         return 0
 
 
-def delete_match(connection, match_id):
+def delete_match(connection: sqlite3.Connection, match_id: int):
+
     cur = connection.cursor()
     cur.execute("DELETE FROM matches WHERE id=?", [match_id])
 
     connection.commit()
 
 
-def delete_bet(connection, bet_id):
+def delete_bet(connection: sqlite3.Connection, bet_id: int):
+
     cur = connection.cursor()
     cur.execute("DELETE FROM bets WHERE id=?", [bet_id])
 
     connection.commit()
 
 
-def delete_bet_by_match_id(connection, match_id):
+def delete_bet_by_match_id(connection: sqlite3.Connection, match_id: int):
+
     bets = get_bet_info_all(connection, match_id)
     for bet in bets:
         add_marbles(connection, bet[3], bet[1])
         delete_bet(connection, bet[0])
 
 
-def add_marbles(connection, player_id, marbles):
+def add_marbles(connection: sqlite3.Connection, player_id: int, marbles: int):
+
     old_marbles = get_marble_count(connection, player_id)
     update_marble_count(connection, player_id, old_marbles + marbles)
 
 
-def add_player_win(connection, player_id, wins):
+def add_player_win(connection: sqlite3.Connection, player_id: int, wins: int):
+
     player_wins = get_player_wins(connection, player_id)
     update_player_wins(connection, player_id, player_wins+wins)
     return
 
 
-def add_player_loses(connection, player_id, loses):
+def add_player_loses(connection: sqlite3.Connection, player_id: int, loses: int):
+
     player_loses = get_player_loses(connection, player_id)
     update_player_loses(connection, player_id, player_loses+loses)
     return
 
 
-def subtract_marbles(connection, player_id, marbles):
+def subtract_marbles(connection: sqlite3.Connection, player_id: int, marbles: int):
+
     old_marbles = get_marble_count(connection, player_id)
 
     if old_marbles - marbles < 0:
@@ -305,7 +343,8 @@ def subtract_marbles(connection, player_id, marbles):
     update_marble_count(connection, player_id, old_marbles - marbles)
 
 
-def transfer_marbles(connection, player_id1, player_id2, marbles):
+def transfer_marbles(connection: sqlite3.Connection, player_id1: int, player_id2: int, marbles: int):
+
     player_marbles1 = get_marble_count(connection, player_id1)
 
     if player_marbles1 < marbles:
@@ -315,7 +354,8 @@ def transfer_marbles(connection, player_id1, player_id2, marbles):
     add_marbles(connection, player_id2, marbles)
 
 
-def is_bet_win(connection, bet_id, winner_id):
+def is_bet_win(connection: sqlite3.Connection, bet_id: int, winner_id: int) -> bool:
+
     bet_info = get_bet_info(connection, bet_id)
 
     if bet_info[4] == winner_id:
@@ -324,7 +364,8 @@ def is_bet_win(connection, bet_id, winner_id):
         return False
 
 
-def process_bets(connection, match_id, winner_id):
+def process_bets(connection: sqlite3.Connection, match_id: int, winner_id: int):
+
     bets = get_bet_info_all(connection, match_id)
 
     bet_count = 0
