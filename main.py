4ebdac72e6a4d89c192f831be1cd9704edbd41f9
WannaDie <consts7s@gmail.com>
2021-05-06 08:17:56 -0400

    Added logging to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 import discord
 import database.database as database
@@ -6,10 +7,20 @@ import database.database as database
 from discord.ext import commands
 from dotenv import load_dotenv
 
-extensions_list = ['cogs.setup', 'cogs.match', 'cogs.bet_control', 'cogs.stats', 'cogs.economy', 'cogs.history']
+logger = logging.getLogger('marble_match')
+logger.setLevel(logging.ERROR)
+file_handler = logging.FileHandler('log.log')
+formatter = logging.Formatter('%(asctime)s : %(module)s : %(levelname)s : %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+logger.debug('Ran')
+
+extensions_list = ['setup', 'match', 'bet_control', 'stats', 'economy', 'history']
 
 load_dotenv()
 if __debug__:
+    logger.setLevel(logging.DEBUG)
     token = os.getenv('DISCORD_DEV_TOKEN')
     database.db_connection = database.create_connection('dev.db')
 else:
@@ -23,7 +34,7 @@ bot = commands.Bot(command_prefix='$', intents=intents, description='Manages Mar
 
 for extension in extensions_list:
     try:
-        bot.load_extension(extension)
+        bot.load_extension(f'cogs.{extension}')
     except Exception as e:
         exc = '{}: {}'.format(type(e).__name__, e)
         print('Failed to load extension {}\n{}'.format(extension, exc))
