f3b5756b7398d6113ef051549dd6e1be91b6e52a
WannaDie <consts7s@gmail.com>
2021-05-12 00:03:37 -0400

    New debug functions

--- marble_match/cogs/debug.py
+++ marble_match/cogs/debug.py
@@ -1,11 +1,13 @@
 import logging
+import operator
 
 import discord
 from discord.ext import commands
 
 import database.database_operation as database_operation
 from database.database_setup import DbHandler
-import utils.account as acc
+import utils.discord_utils as du
+import utils.account as accounts
 import utils.matches as matches
 
 logger = logging.getLogger(f'marble_match.{__name__}')
@@ -19,17 +21,35 @@ class DebugCog(commands.Cog, name='Debug'):
     @commands.command(name='test')
     @commands.guild_only()
     async def test(self, ctx):
-        account1 = acc.get_account(ctx, DbHandler.db_cnc, ctx.author)
-        account2 = acc.get_account(ctx, DbHandler.db_cnc, ctx.author)
-        if account1 == account2:
-            print('true')
+        alist = accounts.get_account_server_all(ctx, DbHandler.db_cnc, ctx.guild.id)
+
+        alist2 = sorted(alist, key=operator.attrgetter('winrate'), reverse=True)
+
+        for acc in alist2:
+            print(f'{acc.winrate:.2f}')
+            x = operator.attrgetter('wins')(acc)
+            print(f'x: {x}')
+
+    @commands.command(name='create_bet_debug')
+    @commands.guild_only()
+    async def create_bet_debug(self, ctx: commands.Context, amount: int, bettor: discord.Member,
+                               bet_target: discord.Member, is_history: bool = False, winner: discord.Member = None,
+                               id_range_start: int = 0, id_range_end: int = 0):
+
+        bettor_id = du.get_id_by_member(ctx, DbHandler.db_cnc, bettor)
+        bet_target_id = du.get_id_by_member(ctx, DbHandler.db_cnc, bet_target)
+        if winner:
+            winner_id = du.get_id_by_member(ctx, DbHandler.db_cnc, winner)
+
+        if id_range_start:
+            for i in range(id_range_start, id_range_end):
+                if is_history:
+                    database_operation.create_bet_history(DbHandler.db_cnc, i, amount, i, bettor_id, bet_target_id,
+                                                          winner_id)
+                else:
+                    database_operation.create_bet(DbHandler.db_cnc, i, amount, i, bettor_id, bet_target_id)
         else:
-            print('false')
-        match = matches.get_match(ctx, 1, True)
-        print(match)
-        match_list = matches.get_matches(ctx, acc.get_account_from_db(ctx, DbHandler.db_cnc, 2))
-        for ma in match_list:
-            print(ma)
+            database_operation.create_bet(DbHandler.db_cnc, None, amount, None, bettor_id, bet_target_id)
 
     @commands.command(name='create_match_debug')
     @commands.guild_only()
@@ -40,8 +60,8 @@ class DebugCog(commands.Cog, name='Debug'):
         logger.debug(f'create_match_debug: {amount}, {active}, {challenger}, {recipient}, {accepted}, {count}')
         x = 0
         while x < count:
-            matches.create_match(ctx, None, amount, acc.get_account(ctx, DbHandler.db_cnc, challenger),
-                                 acc.get_account(ctx, DbHandler.db_cnc, recipient), accepted)
+            matches.create_match(ctx, None, amount, accounts.get_account(ctx, DbHandler.db_cnc, challenger),
+                                 accounts.get_account(ctx, DbHandler.db_cnc, recipient), accepted)
             x += 1
 
 
