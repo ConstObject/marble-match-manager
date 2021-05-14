85f40e7d205dad80f6afda956e4ad807671394d5
WannaDie <consts7s@gmail.com>
2021-05-13 21:18:33 -0400

    Changed database_setup.py to use same logger as all other loggers

--- marble_match/database/database_setup.py
+++ marble_match/database/database_setup.py
@@ -4,14 +4,7 @@ import configparser
 
 from sqlite3 import Error
 
-logger = logging.getLogger(__name__)
-
-logger.setLevel(logging.DEBUG)
-
-file_handler = logging.FileHandler('database.log')
-formatter = logging.Formatter('%(asctime)s : %(module)s : %(levelname)s : %(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+logger = logging.getLogger(f'marble_match.{__name__}')
 
 
 def create_connection(db_file):
