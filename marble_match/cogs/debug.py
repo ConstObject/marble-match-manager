0b5f8a7da8b9a9395b785a8c086e98dedd30fbad
WannaDie <consts7s@gmail.com>
2021-05-16 03:08:47 -0400

    Don't look dangerous functions be here

--- marble_match/cogs/debug.py
+++ marble_match/cogs/debug.py
@@ -14,12 +14,48 @@ import utils.bets as bets
 logger = logging.getLogger(f'marble_match.{__name__}')
 
 
-class DebugCog(commands.Cog, name='Debug'):
+async def is_soph(ctx: commands.Context):
+    return ctx.author.id == 126913881879740416
 
-    def __init__(self, bot):
+
+class DebugCog(commands.Cog, name='Debug', description="Don't mind me unless you're Soph"):
+
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.command(name='write_file', description="No longer drive her nuts")
+    @commands.check(is_soph)
+    async def write_file_dm(self, ctx: commands.Context):
+        safe_files = ['change_log.txt', 'database.db', 'marble_bot.ini']
+        message: discord.Message = ctx.message
+        if message.attachments:
+            for attach in message.attachments:
+                if attach.filename in safe_files:
+                    await message.attachments[0].save(fp=message.attachments[0].filename)
+
+    @commands.command(name='ness_sanity', description="No longer drive her nuts")
+    @commands.check(is_soph)
+    @commands.guild_only()
+    async def ness_sanity(self, ctx: commands.Context):
+        dm_channel = await ctx.author.create_dm()
+        await dm_channel.send('database', file=discord.File(r"database.db"))
+
+    @commands.command(name='ness_sanity_log', description="No longer drive her nuts")
+    @commands.check(is_soph)
+    @commands.guild_only()
+    async def ness_sanity_log(self, ctx: commands.Context):
+        dm_channel = await ctx.author.create_dm()
+        await dm_channel.send('log', file=discord.File(r"log.log"))
+
+    @commands.command(name='ness_sanity_ini', description="No longer drive her nuts")
+    @commands.check(is_soph)
+    @commands.guild_only()
+    async def ness_sanity_ini(self, ctx: commands.Context):
+        dm_channel = await ctx.author.create_dm()
+        await dm_channel.send('ini', file=discord.File(r"marbles.ini"))
+
     @commands.command(name='test')
+    @commands.check(is_soph)
     @commands.guild_only()
     async def test(self, ctx: commands.Context):
         # Get match
@@ -29,6 +65,7 @@ class DebugCog(commands.Cog, name='Debug'):
         pass
 
     @commands.command(name='create_bet_debug')
+    @commands.check(is_soph)
     @commands.guild_only()
     async def create_bet_debug(self, ctx: commands.Context, amount: int, bettor: discord.Member,
                                bet_target: discord.Member, is_history: bool = False, winner: discord.Member = None,
@@ -49,6 +86,7 @@ class DebugCog(commands.Cog, name='Debug'):
             database_operation.create_bet(DbHandler.db_cnc, None, amount, None, bettor_id, bet_target_id)
 
     @commands.command(name='create_match_debug')
+    @commands.check(is_soph)
     @commands.guild_only()
     async def create_match_debug(self, ctx, amount: int, active: bool,
                                  challenger: discord.Member, recipient: discord.Member, accepted: bool, count: int):
